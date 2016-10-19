9f0b1379c67ee3fec8a8903ebe3511722c28a354
Jimmy Arts <j.arts@triple-it.nl>
2016-10-19 09:16:39 +0200

    Fixed optional issue

--- Sources/Base64.swift
+++ Sources/Base64.swift
@@ -4,7 +4,10 @@ import Foundation
 /// URI Safe base64 encode
 func base64encode(_ input:Data) -> String {
   let data = input.base64EncodedData(options: NSData.Base64EncodingOptions(rawValue: 0))
-  let string = String(describing: NSString(data: data, encoding: String.Encoding.utf8.rawValue))
+  guard let cocoaString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else {
+    return ""
+  }
+  let string = String(describing: cocoaString)
   return string
     .replacingOccurrences(of: "+", with: "-", options: NSString.CompareOptions(rawValue: 0), range: nil)
     .replacingOccurrences(of: "/", with: "_", options: NSString.CompareOptions(rawValue: 0), range: nil)
@@ -14,15 +17,15 @@ func base64encode(_ input:Data) -> String {
 /// URI Safe base64 decode
 func base64decode(_ input:String) -> Data? {
   let rem = input.characters.count % 4
-
+  
   var ending = ""
   if rem > 0 {
     let amount = 4 - rem
     ending = String(repeating: "=", count: amount)
   }
-
+  
   let base64 = input.replacingOccurrences(of: "-", with: "+", options: NSString.CompareOptions(rawValue: 0), range: nil)
     .replacingOccurrences(of: "_", with: "/", options: NSString.CompareOptions(rawValue: 0), range: nil) + ending
-
+  
   return Data(base64Encoded: base64, options: NSData.Base64DecodingOptions(rawValue: 0))
 }
